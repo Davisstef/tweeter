2267536ca4f227545cc9568eb2ee941c236be67c
Stefan Darrell Davis <60591525+Davisstef@users.noreply.github.com>
2022-03-26 00:51:17 -0400

    Update composer-char-counter.js

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,10 +1,15 @@
+// Counts the number of characters typed in and changes their color
 $(document).ready(function() {
+  
   $('textarea').on('input', function (event) {
     let len = $(this).val().length
     let $counter= $(this).parent('form').find('.counter')
+    
     updateCountdown($counter, len)
   })
+  
 })
+
 function updateCountdown($counter, len) {
   let charsLeft = 140 - len
   $counter.text(charsLeft)
@@ -12,4 +17,5 @@ function updateCountdown($counter, len) {
   if (charsLeft < 0) {
     $counter.css('color', 'red')
   }
+  
 };
